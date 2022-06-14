b29b1f7260299b6b178f9c225814911584348cb3
¨mago¨ <martingramajo08@gmail.com>
2022-06-14 02:38:03 -0300

    agrego el state loading para el spinner

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import Home from './pages/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PersonajePageDetalle from './pages/PersonajePageDetalle';
 import NavReact from './components/NavReact';
+import { useState } from 'react';
 
 function App() {
+  const [loading, setLoading] = useState(false);
   return (
     <BrowserRouter>
       <NavReact />
       <Routes>
         <Route
           path="/"
-          element={<Home />} />
-          <Route path="/character/:id" element={<PersonajePageDetalle />} />
+          element={<Home loading={loading} setLoading={setLoading} />} />
+          <Route path="/character/:id" element={<PersonajePageDetalle loading={loading} />} />
       </Routes>
 
     </BrowserRouter>
