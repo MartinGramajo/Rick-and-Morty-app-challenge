967de3c0bee295315ce381b10e72fbe6bc164236
¨mago¨ <martingramajo08@gmail.com>
2022-06-14 02:46:17 -0300

    agrego footer en app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PersonajePageDetalle from './pages/PersonajePageDetalle';
 import NavReact from './components/NavReact';
 import { useState } from 'react';
+import Footer from './components/Footer';
 
 function App() {
   const [loading, setLoading] = useState(false);
@@ -16,7 +17,7 @@ function App() {
           element={<Home loading={loading} setLoading={setLoading} />} />
           <Route path="/character/:id" element={<PersonajePageDetalle loading={loading} />} />
       </Routes>
-
+      <Footer />
     </BrowserRouter>
   );
 }
