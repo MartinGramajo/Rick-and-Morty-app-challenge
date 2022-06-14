a08f89270996d2c3248da07019c50d13e4da1026
¨mago¨ <martingramajo08@gmail.com>
2022-06-14 02:22:30 -0300

    agrego la page personajeDetalle

---
+++ src/pages/PersonajePageDetalle.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PersonajeDetalle from '../components/PersonajeDetalle'
+
+export default function PersonajePageDetalle() {
+  return (
+      <div>
+          <PersonajeDetalle />
+    </div>
+  )
+}
