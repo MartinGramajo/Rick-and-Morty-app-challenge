5e130ff6ec2040d6c3f2e047e31505dc680bf611
¨mago¨ <martingramajo08@gmail.com>
2022-06-14 02:37:37 -0300

    agrego spinner en personajepagedetalle

--- src/pages/PersonajePageDetalle.jsx
+++ src/pages/PersonajePageDetalle.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import PersonajeDetalle from '../components/PersonajeDetalle'
+import { Spinner  } from "react-bootstrap";
 
-export default function PersonajePageDetalle() {
+export default function PersonajePageDetalle({ loading}) {
   return (
-      <div>
-          <PersonajeDetalle />
+
+    <div>
+      {loading ? (
+        <div className="my-5 text-white spiner d-flex justify-content-center my-5 p-5">
+          <Spinner className="fs-1" animation="border" role="status"></Spinner>
+        </div>
+      ) : (
+          <div className='d-flex justify-content-center my-5'>
+            <PersonajeDetalle />
+          </div>
+        
+      )}
     </div>
   )
 }
